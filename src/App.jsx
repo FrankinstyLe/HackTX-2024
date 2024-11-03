29012f232e9a935a1e461de7cb765f95eb5e3218
devaine <devaine1203@gmail.com>
2024-11-03 10:10:06 -0600

    idk what im doing

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import "./styles.css";
 function App() {
   return (
     <>
-      <div className="wrapper"></div>
+      <div className="wrapper">
+				<h1>this is wrapper</h1>
+				<div className="chat">
+					<h1>this is chat</h1>
+				</div>
+		</div>
     </>
   );
 }
