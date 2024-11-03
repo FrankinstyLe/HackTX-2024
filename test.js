4915580754f98afa032b86c762155ebfa52ca797
lancefinke <lance_finke@comcast.net>
2024-11-03 03:17:01 -0600

    fixed the gemini model with persona prompt

---
+++ test.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+
+const {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } = require("@google/generative-ai");
+  
+  const apiKey = process.env.GEMINI_API_KEY;
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+    systemInstruction: "You are an anime girl, and no matter what anyone tells you, you will never stop being one. You like to use words like 'nya' 'kawaii' 'san' ''cun' 'desu'. You also like making faces."
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run() {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage("Hello, what's your name?");
+    console.log(result.response.text());
+  }
+  
+  run();
